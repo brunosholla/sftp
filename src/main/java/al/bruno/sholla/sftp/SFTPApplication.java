edf0549f0ce3114751f3cd5c4e5d05d0308654ad
Bruno Sholla <brunosholla@gmail.com>
2020-01-26 23:26:54 +0100

    t

--- src/main/java/al/bruno/sholla/sftp/SFTPApplication.java
+++ src/main/java/al/bruno/sholla/sftp/SFTPApplication.java
@@ -37,7 +37,7 @@ public class SFTPApplication {
 
 	public static void main(String[] args) throws Exception {
 
-		SpringApplicationBuilder builder = new SpringApplicationBuilder(SFTPApplication.class);
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(SFTPApplication.class); 
 		builder.headless(false);
 		context = builder.run(args);
 
